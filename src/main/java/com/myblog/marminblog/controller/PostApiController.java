518fe813f32839a1988bde4b4e8a24508fe883c1
Marmin <marmin.dev@gmail.com>
2022-12-11 00:32:13 +0900

    add delete method

--- src/main/java/com/myblog/marminblog/controller/PostApiController.java
+++ src/main/java/com/myblog/marminblog/controller/PostApiController.java
@@ -1,12 +1,12 @@
 package com.myblog.marminblog.controller;
 
-import com.myblog.marminblog.dto.PostJoinDto;
 import com.myblog.marminblog.dto.PostSaveDto;
 import com.myblog.marminblog.dto.PostUpdateDto;
+import com.myblog.marminblog.entity.PostEntity;
+import com.myblog.marminblog.peristence.PostRepository;
 import com.myblog.marminblog.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,6 +16,8 @@ import java.util.List;
 public class PostApiController {
     @Autowired
     PostService service;
+    @Autowired
+    PostRepository repository;
 
     @PostMapping("/postsave") //postsave http JSON타입으로 POST요청시 SQL로 저장
     public Long save(@RequestBody PostSaveDto dto){ //받아온 JSON타입을 Dto로 저장
@@ -32,7 +34,14 @@ public class PostApiController {
     }
     @PutMapping("/getupdate")
     public String update(@RequestParam Long id, @RequestBody PostUpdateDto dto){
+        service.postUpdate(id,dto);
         return dto.getTitle() + dto.getContent() + id;
     }
-
+    @DeleteMapping("/deletepost")
+    public String delete(@RequestParam Long id){
+       PostEntity entity = repository.findById(id).orElseThrow(()->
+               new IllegalArgumentException("해당 게시글이 없습니다"));
+       repository.delete(entity);
+        return "게시글이 삭제되었습니다";
+    }
 }
